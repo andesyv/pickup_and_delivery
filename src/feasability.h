b4461b150a20340c0298589704873ad65f074d22
Anders Syvertsen <andesyv@gmail.com>
2021-02-12 20:25:00 +0100

    Route timewindow feasability
    For the records: The last commit was supposed to say "Route capacity
    feasability"

--- src/feasability.h
+++ src/feasability.h
@@ -40,6 +40,58 @@ std::optional<std::runtime_error> checkFeasability(const Problem& problem, const
             if (capacity < 0)
                 capacity = 0;
         }
+
+        auto findPath = [&](const auto& a, const auto& b) {
+            auto it = std::find_if(problem.trips.begin(), problem.trips.end(), [&](const Trip& t){ return t.origin == a && t.destination == b; });
+            return (it != problem.trips.end()) ? std::optional<Trip>{*it} : std::nullopt;
+        };
+
+        // Route timeslots
+        currentCalls.clear();
+        int time{vehicle.startingTime}; // Time elapsed since start
+        int currentNode{vehicle.homeNodeIndex};
+        /** Logic:
+         * When adding route, first traverse to route node.
+         * When removing route, traverse to end node for removed route.
+         */
+        for (auto callIndex : route) {
+            const auto& call = problem.calls.at(callIndex);
+            auto search = std::find(currentCalls.begin(), currentCalls.end(), callIndex);
+            if (search == currentCalls.end()) {
+                currentCalls.push_back(callIndex);
+                // Since adding new route, traverse to start of route.
+                auto path = findPath(currentNode, call.origin);
+                if (!path)
+                    return std::runtime_error{"Could not find route path."};
+                time += path.value().time;
+                currentNode = path.value().destination;
+
+                // Check that we didn't miss the pickup window.
+                if (call.upperTimewindowPickup < time)
+                    return std::runtime_error{std::string{"Vehicle "}.append(std::to_string(i)).append(" missed their timeslot for pickup.")};
+
+                // Wait for pickup timewinow
+                if (time < call.lowerTimewindowDelivery)
+                    time = call.lowerTimewindowDelivery;
+            } else {
+                auto path = findPath(currentNode, call.destination);
+                if (!path)
+                    return std::runtime_error{"Could not find route path."};
+                time += path.value().time;
+                currentNode = path.value().destination;
+
+                // Assume that we deliver the instant we arrive
+                // Check that we didn't miss our delivery window
+                if (call.upperTimewindowDelivery < time)
+                    return std::runtime_error{std::string{"Vehicle "}.append(std::to_string(i)).append(" missed their timeslot for delivery.")};
+                
+                // Wait for delivery
+                if (time < call.lowerTimewindowDelivery)
+                    time = call.lowerTimewindowDelivery;
+                
+                currentCalls.erase(search);
+            }
+        }
     }
 
     return std::nullopt;
