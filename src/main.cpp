3732b6527a216cd38c6a2c7da3de4ee9b1a636d3
Anders Syvertsen <andesyv@gmail.com>
2021-02-28 22:43:59 +0100

    Added compiler options for printing of results
    Cluttering up my main.cpp file, but atleast still keeping the compile cost low

--- src/main.cpp
+++ src/main.cpp
@@ -1,150 +1,232 @@
 #include <iostream>
+#include <iomanip>
 #include <vector>
 #include "problem.h"
 #include "cost.h"
 #include <chrono>
 #include "heuristics.h"
+#ifdef PARALLEL_EXECUTION
+#pragma message("PARALLEL_EXECUTION is enabled")
 #include <thread>
 #include <mutex>
+#endif
 #include <future>
 
-int main(int argc, char *argv[]) {
+#ifdef FILE_OUTPUT
+#pragma message("Outputting to file is enabled")
+#include <fstream>
+#include <filesystem>
+#endif
+
+int main(int argc, char *argv[])
+{
     std::cout << "Hello world!" << std::endl;
 
+#ifdef FILE_OUTPUT
+    // Output file setup
+    std::ofstream outf{"output.csv", std::ofstream::trunc | std::ofstream::out};
+    if (!outf) {
+        std::cout << "Failed to open output file \"output.csv\". Exiting." << std::endl;
+        return 1;
+    }
+#endif
+
+    // Input files:
     std::vector files{
         "./data/Call_7_Vehicle_3.txt",
         "./data/Call_18_Vehicle_5.txt",
         "./data/Call_035_Vehicle_07.txt",
         "./data/Call_080_Vehicle_20.txt",
-        "./data/Call_130_Vehicle_40.txt"
-    };
+        "./data/Call_130_Vehicle_40.txt"};
 
     // Possibility to run with argument paths aswell
     // If that is the case, use them instead
-    if (1 < argc) {
+    if (1 < argc)
+    {
         files.clear();
         for (int i{1}; i < argc; ++i)
             files.push_back(argv[i]);
     }
 
-    for (const auto& file : files) {
-        
-        std::cout << std::endl << std::endl << "File: " << file << std::endl;
-        long long totalTime{0};
-        long long totalCost{0};
-        int bestCost{std::numeric_limits<int>::max()};
-        std::vector<std::vector<int>> bestSolution{};
+    for (const auto &file : files)
+    {
+        std::cout << std::endl << file << std::endl;
 
+#ifdef FILE_OUTPUT
+        // If file output is enabled, write some file handle info at the top
+        outf << std::filesystem::path{file}.filename().string() << std::endl;
+        outf << ",\"Average objective\",\"Best objective\",\"Improvement (%)\",\"Running time\",\"Best solution\"" << std::endl;
+#endif
+
+        // Problem loading
         auto pResult = load(file);
-        if (!pResult) {
+        if (!pResult)
+        {
             std::cout << pResult.err().what() << std::endl;
             return 1;
         }
         const auto problem = pResult.val();
 
-        constexpr auto THREAD_COUNT = 10;
-        std::vector<std::thread> threads{};
-        threads.reserve(THREAD_COUNT);
-        std::vector<std::future<int>> returnVals;
-        std::mutex m;
+#ifdef ALL_ALGORITHMS
+#pragma message("Building with all search algorithms")
 
-        const auto loop = [&](std::promise<int>&& p) {
-            std::chrono::steady_clock::time_point t1{std::chrono::steady_clock::now()};
-            const auto solution = localSearch(problem);
-            auto duration = std::chrono::steady_clock::now() - t1;
-
-            // Scope so mutex lock can do it's thing.
-            {
-                std::lock_guard lock{m};
-
-                std::cout << "Best solution: ";    
-                for (auto l = fromNestedList(solution); auto v : l) {
-                    std::cout << std::to_string(v) << ",";
-                }
-                std::cout << std::endl;
-                auto cResult = getCost(problem, solution);
-                if (!cResult) {
-                    std::cout << cResult.err().what() << std::endl;
-                    p.set_value(1);
-                    return;
-                }
-                const auto cost = cResult.val();
-                if (cost < bestCost) {
-                    bestCost = cost;
-                    bestSolution = solution;
-                }
-                std::cout << "Cost: " << cost << std::endl;
-                totalCost += cost;
-                
-                const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-                totalTime += ms;
-
-                std::cout << "runtime: " << ms << "ms" << std::endl;
-            }
-
-            // Remember to set return value
-            p.set_value(0);
+        const std::vector searchAlgorithms{
+            std::make_pair(blindRandomSearch, "Random Search"),
+            std::make_pair(localSearch, "Local Search"),
+            std::make_pair(simulatedAnnealing, "Simulated Annealing")
         };
 
-        // Init promises (for return values for each thread)
-        std::vector<std::promise<int>> promises;
-        promises.resize(THREAD_COUNT);
-        returnVals.reserve(THREAD_COUNT);
+        for (const auto& [search, algname] : searchAlgorithms)
+        {
+            std::cout << "Algorithm: " << algname << std::endl;
 
-#ifdef PARALLEL_EXECUTION
-#pragma message("PARALLEL_EXECUTION is enabled")
-
-        // Init threads
-        for (auto i{0}; i < THREAD_COUNT; ++i) {
-            auto p = std::move(promises[i]);
-            returnVals.push_back(p.get_future());
-            threads.push_back(std::thread{loop, std::move(p)});
-        }
-
-        // Wait for all threads to finish
-        // for (auto& [t, f] : Zip{threads, returnVals}) {
-        //     t.join();
-        //     if (0 != f.get())
-        //         return f.get();
-        // }
-
-        auto [tit, rit] = std::make_tuple(threads.begin(), returnVals.begin());
-        for (; tit != threads.end() && rit != returnVals.end(); ++tit, ++rit) {
-            auto& t = *tit;
-            auto& f = *rit;
-            t.join();
-            if (0 != f.get())
-                return f.get();
-        }
-
-#else
-        // If non-multithread, run loops manually
-        for (auto i{0}; i < THREAD_COUNT; ++i) {
-            auto p = std::move(promises[i]);
-            auto f = p.get_future();
-            loop(std::move(p));
-            if (0 != f.get())
-                return f.get();
-        }
 #endif
 
-        auto improvementPercent = [&](){
-            auto initCost = getCost(problem, genInitialSolution(problem));
-            auto bestCost = getCost(problem, bestSolution);
+            long long totalTime{0};
+            long long totalCost{0};
+            int bestCost{std::numeric_limits<int>::max()};
+            std::vector<std::vector<int>> bestSolution{};
 
-            return (initCost && bestCost) ? 100.0 * (initCost.val() - bestCost.val()) / initCost.val() : 0.0;
-        };
+            constexpr auto THREAD_COUNT = 10;
 
-        std::cout << std::endl << std::endl;
+#ifdef PARALLEL_EXECUTION
+            std::vector<std::thread> threads{};
+            threads.reserve(THREAD_COUNT);
+            std::mutex m;
+#endif
 
-        std::cout << "Average cost: " << static_cast<double>(totalCost) / 10.0 << std::endl;
-        std::cout << "Average runtime: " << static_cast<double>(totalTime) / 10.0 << "ms" << std::endl;
-        std::cout << "Best cost: " << bestCost << std::endl;
-        std::cout << "Best solution: ";
-        for (const auto& v : fromNestedList(bestSolution))
-            std::cout << v << ", ";
-        std::cout << "Improvement (%): " << std::to_string(improvementPercent()) << std::endl;
-        
+            std::vector<std::future<int>> returnVals;
+
+            const auto loop = [&](std::promise<int> &&p) {
+                std::chrono::steady_clock::time_point t1{std::chrono::steady_clock::now()};
+
+#ifdef ALL_ALGORITHMS
+                const auto solution = search(problem);
+#else
+                const auto solution = localSearch(problem);
+#endif
+
+                auto duration = std::chrono::steady_clock::now() - t1;
+
+                // Scope so mutex lock can do it's thing.
+                {
+#ifdef PARALLEL_EXECUTION
+                    std::lock_guard lock{m};
+#endif
+
+                    std::cout << "Best solution: ";
+                    for (auto l = fromNestedList(solution); auto v : l)
+                    {
+                        std::cout << std::to_string(v) << ",";
+                    }
+                    std::cout << std::endl;
+                    auto cResult = getCost(problem, solution);
+                    if (!cResult)
+                    {
+                        std::cout << cResult.err().what() << std::endl;
+                        p.set_value(1);
+                        return;
+                    }
+                    const auto cost = cResult.val();
+                    if (cost < bestCost)
+                    {
+                        bestCost = cost;
+                        bestSolution = solution;
+                    }
+                    std::cout << "Cost: " << cost << std::endl;
+                    totalCost += cost;
+
+                    const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+                    totalTime += ms;
+
+                    std::cout << "runtime: " << ms << "ms" << std::endl;
+                }
+
+                // Remember to set return value
+                p.set_value(0);
+            };
+
+            // Init promises (for return values for each thread)
+            std::vector<std::promise<int>> promises;
+            promises.resize(THREAD_COUNT);
+            returnVals.reserve(THREAD_COUNT);
+
+#ifdef PARALLEL_EXECUTION
+
+            // Init threads
+            for (auto i{0}; i < THREAD_COUNT; ++i)
+            {
+                auto p = std::move(promises[i]);
+                returnVals.push_back(p.get_future());
+                threads.push_back(std::thread{loop, std::move(p)});
+            }
+
+            // Wait for all threads to finish
+            // for (auto& [t, f] : Zip{threads, returnVals}) {
+            //     t.join();
+            //     if (0 != f.get())
+            //         return f.get();
+            // }
+
+            auto [tit, rit] = std::make_tuple(threads.begin(), returnVals.begin());
+            for (; tit != threads.end() && rit != returnVals.end(); ++tit, ++rit)
+            {
+                auto &t = *tit;
+                auto &f = *rit;
+                t.join();
+                if (0 != f.get())
+                    return f.get();
+            }
+
+#else
+            // If non-multithread, run loops manually
+            for (auto i{0}; i < THREAD_COUNT; ++i)
+            {
+                auto p = std::move(promises[i]);
+                auto f = p.get_future();
+                loop(std::move(p));
+                if (0 != f.get())
+                    return f.get();
+            }
+#endif
+
+            auto improvementPercent = [&]() {
+                auto initCost = getCost(problem, genInitialSolution(problem));
+                auto bestCost = getCost(problem, bestSolution);
+
+                return (initCost && bestCost) ? 100.0 * (initCost.val() - bestCost.val()) / initCost.val() : 0.0;
+            };
+
+            std::cout << std::endl
+                      << std::endl;
+
+            std::cout << "Average cost: " << static_cast<double>(totalCost) / 10.0 << std::endl;
+            std::cout << "Average runtime: " << static_cast<double>(totalTime) / 10.0 << "ms" << std::endl;
+            std::cout << "Best cost: " << bestCost << std::endl;
+            std::cout << "Best solution: ";
+            for (const auto &v : fromNestedList(bestSolution))
+                std::cout << v << ", ";
+            std::cout << "Improvement (%): " << std::to_string(improvementPercent()) << std::endl;
+
+#ifdef ALL_ALGORITHMS
+#ifdef FILE_OUTPUT
+            // Write results to file:
+            outf << "\"" << algname << "\",";
+            outf << totalCost / 10 << "," << bestCost << ",";
+            outf << std::fixed << std::setprecision(2) << improvementPercent() << ",";
+            outf << static_cast<double>(totalTime) / 10.0 << "ms" << ",";
+            for (const auto &v : fromNestedList(bestSolution))
+                outf << v << " ";
+            outf << std::endl;
+
+        }
+
+        outf << std::endl;
+#else
+        }
+#endif
+#endif
+
     }
 
     return 0;
